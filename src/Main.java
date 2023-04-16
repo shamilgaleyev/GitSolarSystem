ad307fa27d8c7ebb580909ccb7f33672939d9c03
shamilgaleyev <galeyevshamil@gmail.com>
2023-04-15 22:14:29 -0400

    issue

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         solar.planets="2";
         solar.moon = "2";
         solar.sun = "1";
+
+        sun fero = new sun();
+        fero.color = "red";
+        fero.temperature = 34234;
+        fero.heat = "21423546k";
     }
 }
